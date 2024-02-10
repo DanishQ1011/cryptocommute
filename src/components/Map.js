437fd50d7aef3e9cd5b7772c924790b46653d2c9
Shah Danish <shah.official1011@gmail.com>
2024-02-10 20:02:33 +0530

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import mapboxgl from 'mapbox-gl';
+import { useEffect } from 'react';
 
-const Map = () => {
-  return (
-    <div>Map</div>
-  )
+const style = {
+  wrapper: `flex-1 h-full w-full`,
 }
 
-export default Map
+const Map = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/shah1011/clsg5xu99019n01qq02fabk0v',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
+  }, [])
+
+  return <div className={style.wrapper} id='map'/>
+  
+}
+
+export default Map 
