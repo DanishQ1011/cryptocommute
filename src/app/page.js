7774e515cf3e375393a11ba496ee7594d75044d3
Shah Danish <shah.official1011@gmail.com>
2024-02-11 15:18:23 +0530

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ const style = {
   wrapper: `h-screen w-screen flex flex-col`,
   main: `h-full w-screen flex-1 z-10`,
   mapContainer: `flex-1 w-full h-full`,
-  rideRequestContainer: `h-[600px] w-[400px] ml-[4rem] mt-[6rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
-  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
+  rideRequestContainer: `h-[600px] w-[400px] ml-[4rem] mt-[6rem] absolute shadow-xl top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col`,
 }
 
 export default function Home() {
